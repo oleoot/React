8e9a6251cbf3a7754d19855c71a49c6a881ccff6
oleoot <oleoot28@gmail.com>
2020-02-18 12:48:43 +0200

    Increment

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Tweet from "./Tweet"
 
 
 
 
 function App(){
+
+const [isRed, setRed] = useState(false)
+const [count, setCount] = useState(0);
+
+const increment = () =>{
+ setCount(count + 1);
+}
+
 return (
 <div className="app">
-<Tweet name="Oleg Kuts" message="Im am tired"/>
+<button onClick={increment}>Increment</button>
+<h1>{count}</h1>
+
+{/* <Tweet name="Oleg Kuts" message="Im am tired"/>
 <Tweet name="Alex Costa" message="Try my new hair styler"/>
-<Tweet name="Bill Gates" message="New toilets will be deployed next month"/>
+<Tweet name="Bill Gates" message="New toilets will be deployed next month"/> */}
 </div>
 )
 }
